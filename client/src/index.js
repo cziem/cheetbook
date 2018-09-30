9219afc63ed36fadd67e89291201d448432e1e3d
Fay Vor <phavorsparks@gmail.com>
2018-09-30 22:48:19 +0100

    Add client-side files

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 
+'react-dom'
+import 'normalize.css/normalize.css'
+import App from './components/App'
+
+import './styles/main.scss'
+
+const Root = document.getElementById('root')
+ReactDOM.render(<App />, Root)
